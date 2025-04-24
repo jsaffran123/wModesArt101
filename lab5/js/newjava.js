944fc08e8fcb0e0a63881a24a099f320033499d4
jsaffran123 <jsaffran@ucsc.edu>
2025-04-23 21:06:47 -0700

    LETS GOOO

---
+++ lab5/js/newjava.js
@@ -0,0 +1,21 @@
+/**
+ * Author:  Jonah Saffran
+ * Created: 04/23/2025
+ * 
+ **/
+
+// Define Variables
+make = "Subaru"
+model = "crosstrek"
+color = "grey"
+year = "2024"
+
+// Calculate
+age = 2025 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Color: " + color + "<br>");
